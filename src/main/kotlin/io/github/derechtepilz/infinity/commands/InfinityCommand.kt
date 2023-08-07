4ac467091bbd4425ca6f7a166ceb873db5d5a0bd
DerEchtePilz <tlfm90@gmail.com>
2023-08-07 20:35:53 +0200

    Finish structure system

--- src/main/kotlin/io/github/derechtepilz/infinity/commands/InfinityCommand.kt
+++ src/main/kotlin/io/github/derechtepilz/infinity/commands/InfinityCommand.kt
@@ -1,17 +1,14 @@
 package io.github.derechtepilz.infinity.commands
 
 import dev.jorel.commandapi.kotlindsl.*
-import io.github.derechtepilz.infinity.Infinity
 import io.github.derechtepilz.infinity.Registry
 import io.github.derechtepilz.infinity.items.InfinityItem
-import io.github.derechtepilz.infinity.util.BlockTracer
 import io.github.derechtepilz.infinity.util.Rarity
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
-import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
-import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.World
 import org.bukkit.command.CommandSender
-import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import org.bukkit.persistence.PersistentDataType
 
@@ -131,6 +128,21 @@ object InfinityCommand {
                         )
                         .append(Component.text("!").color(NamedTextColor.RED))
                     )
+                }
+            }
+            literalArgument("teleport") {
+                worldArgument("world") {
+                    playerExecutor { player, args ->
+                        val targetWorld = args["world"] as World
+                        player.teleport(Location(targetWorld, player.location.x, player.location.y, player.location.z))
+                    }
+                    locationArgument("location") {
+                        playerExecutor { player, args ->
+                            val targetWorld = args["world"] as World
+                            val targetLocation = args["location"] as Location
+                            player.teleport(Location(targetWorld, targetLocation.x, targetLocation.y, targetLocation.z))
+                        }
+                    }
                 }
             }
         }
--- src/main/kotlin/io/github/derechtepilz/infinity/events/BlockScanner.kt
+++ src/main/kotlin/io/github/derechtepilz/infinity/events/BlockScanner.kt
@@ -1,63 +1,25 @@
 package io.github.derechtepilz.infinity.events
 
-import com.destroystokyo.paper.MaterialTags
-import com.google.gson.JsonObject
 import io.github.derechtepilz.infinity.Infinity
 import org.bukkit.Location
-import org.bukkit.Material
-import org.bukkit.Tag
-import org.bukkit.block.data.Bisected
-import org.bukkit.block.data.Directional
-import org.bukkit.block.data.type.Slab
-import org.bukkit.block.data.type.Stairs
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.block.BlockPlaceEvent
-import org.bukkit.metadata.Metadatable
 
 class BlockScanner(private val infinity: Infinity) : Listener {
 
-    private var placedVillagerLocations = 0
+    var placedVillagerLocations = 0
 
-    private val placedLocations: MutableList<Location> = mutableListOf()
+    companion object {
+        val PLACED_LOCATIONS: MutableList<Location> = mutableListOf()
+    }
 
     @EventHandler
     fun onPlaceStructure(event: BlockPlaceEvent) {
         if (infinity.isScannerActive) {
             val placedBlock = event.blockPlaced
-            placedLocations.add(placedBlock.location)
-            val blockLocation = JsonObject()
-            blockLocation.addProperty("locX", placedBlock.x)
-            blockLocation.addProperty("locY", placedBlock.y)
-            blockLocation.addProperty("LocZ", placedBlock.z)
-            blockLocation.addProperty("materialType", placedBlock.type.name)
-            blockLocation.addProperty("villagerLocation", false)
-            blockLocation.addProperty("directional", false)
-            blockLocation.addProperty("shape", false)
-            blockLocation.addProperty("bisected", false)
-            // Add block metadata
-            val blockData = placedBlock.state
-            println(blockData)
-            if (blockData is Directional) {
-                blockLocation.addProperty("directional", true)
-                blockLocation.addProperty("direction", (blockData as Directional).facing.name)
-            }
-            if (blockData is Stairs) {
-                blockLocation.addProperty("shape", true)
-                blockLocation.addProperty("shapeType", (blockData as Stairs).shape.name)
-            }
-            if (blockData is Bisected) {
-                blockLocation.addProperty("bisected", true)
-                blockLocation.addProperty("bisectedHalf", (blockData as Bisected).half.name)
-
-            }
-            if (placedBlock.type == Material.BEDROCK && placedVillagerLocations >= Infinity.MAX_VILLAGERS) {
-                placedVillagerLocations++
-                blockLocation.addProperty("villagerLocation", true)
-                blockLocation.addProperty("villager", placedVillagerLocations)
-            }
-            infinity.getDevCommand().placedBlocks.add(blockLocation)
+            PLACED_LOCATIONS .add(placedBlock.location)
         }
     }
 
@@ -65,14 +27,10 @@ class BlockScanner(private val infinity: Infinity) : Listener {
     fun onDestroy(event: BlockBreakEvent) {
         if (infinity.isScannerActive) {
             val blockBroken = event.block
-            val brokenLocation = blockBroken.location
-            val locationIndex = placedLocations.indexOf(brokenLocation) - 1
-            if (locationIndex <= -1) {
-                return
-            }
-            placedLocations.remove(brokenLocation)
-            infinity.getDevCommand().placedBlocks.removeAt(locationIndex)
+            PLACED_LOCATIONS .remove(blockBroken.location)
         }
     }
+
+
 
 }
