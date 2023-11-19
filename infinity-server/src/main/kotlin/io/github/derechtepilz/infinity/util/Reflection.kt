8120b50ae44f5d322c09ef6a0d9429bf456d84c8
DerEchtePilz <tlfm90@gmail.com>
2023-11-19 18:59:12 +0100

    Migrate to Brigadier directly instead of CommandAPI

--- infinity-server/src/main/kotlin/io/github/derechtepilz/infinity/util/Reflection.kt
+++ infinity-server/src/main/kotlin/io/github/derechtepilz/infinity/util/Reflection.kt
@@ -1,5 +1,6 @@
 package io.github.derechtepilz.infinity.util
 
+import net.minecraft.server.dedicated.DedicatedServer
 import net.minecraft.server.level.ServerPlayer
 import org.bukkit.Bukkit
 import java.util.UUID
@@ -13,6 +14,9 @@ object Reflection {
 	fun getServerPlayer(uuid: UUID): ServerPlayer {
 		return Class.forName(getCraftBukkitClass("entity.CraftPlayer")).getMethod("getHandle").invoke(Bukkit.getPlayer(uuid)) as ServerPlayer
 	}
+
+	@JvmStatic
+	fun getDedicatedServer() = Class.forName(getCraftBukkitClass("CraftServer")).cast(Bukkit.getServer()) as DedicatedServer
 
 	private fun getCraftBukkitClass(name: String): String {
 		return "$CRAFTBUKKIT_PACKAGE.$name"
