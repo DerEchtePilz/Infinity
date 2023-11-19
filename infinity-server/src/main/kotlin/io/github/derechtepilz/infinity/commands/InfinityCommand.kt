49a071646cf2b05facdd5bedd7878f35f5045801
DerEchtePilz <tlfm90@gmail.com>
2023-11-19 18:59:12 +0100

    Migrate to Brigadier directly instead of CommandAPI

--- infinity-server/src/main/kotlin/io/github/derechtepilz/infinity/commands/InfinityCommand.kt
+++ infinity-server/src/main/kotlin/io/github/derechtepilz/infinity/commands/InfinityCommand.kt
@@ -18,6 +18,15 @@
 
 package io.github.derechtepilz.infinity.commands
 
+import com.mojang.brigadier.LiteralMessage
+import com.mojang.brigadier.arguments.IntegerArgumentType
+import com.mojang.brigadier.builder.ArgumentBuilder
+import com.mojang.brigadier.builder.LiteralArgumentBuilder
+import com.mojang.brigadier.builder.RequiredArgumentBuilder
+import com.mojang.brigadier.context.CommandContext
+import com.mojang.brigadier.exceptions.CommandSyntaxException
+import com.mojang.brigadier.exceptions.SimpleCommandExceptionType
+import com.mojang.brigadier.tree.ArgumentCommandNode
 import dev.jorel.commandapi.CommandAPI
 import dev.jorel.commandapi.kotlindsl.*
 import io.github.derechtepilz.infinity.Infinity
@@ -30,74 +39,47 @@ import io.github.derechtepilz.infinity.gamemode.switching.terminateStoryTitleTas
 import io.github.derechtepilz.infinity.items.InfinityItem
 import io.github.derechtepilz.infinity.items.Rarity
 import io.github.derechtepilz.infinity.util.Keys
+import io.github.derechtepilz.infinity.util.Reflection
 import io.github.derechtepilz.infinity.util.capitalize
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.minimessage.MiniMessage
+import net.minecraft.commands.CommandSourceStack
+import net.minecraft.commands.arguments.DimensionArgument
+import net.minecraft.commands.arguments.coordinates.Coordinates
+import net.minecraft.commands.arguments.coordinates.Vec3Argument
+import net.minecraft.resources.ResourceLocation
 import org.bukkit.Location
 import org.bukkit.World
 import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 import org.bukkit.event.player.PlayerTeleportEvent
 import org.bukkit.inventory.ItemStack
 import org.bukkit.persistence.PersistentDataType
+import java.util.function.Predicate
 
 object InfinityCommand {
 
 	fun register() {
-		commandTree("infinity") {
-			literalArgument("give") {
-				withRequirement { sender: CommandSender -> sender.isOp }
-				literalArgument("*") {
-					playerExecutor { player, _ ->
-						player.inventory.addItem(*Registry.Item.getAllItems())
+		val itemIds = Registry.Item.getItemIds()
+		val requirePlayer = Predicate<CommandSourceStack> { stack -> stack.source.getBukkitSender(stack) is Player }
+		val requirePlayerOperator = Predicate<CommandSourceStack> { stack -> stack.source.getBukkitSender(stack) is Player && stack.source.getBukkitSender(stack).isOp }
+
+		Reflection.getDedicatedServer().commands.dispatcher.register(LiteralArgumentBuilder.literal<CommandSourceStack>("infinity")
+			.then(LiteralArgumentBuilder.literal<CommandSourceStack>("give")
+				.requires(requirePlayerOperator)
+				.then(LiteralArgumentBuilder.literal<CommandSourceStack>("*")
+					.executes { ctx ->
+						val sender = getSenderAsPlayer(ctx)
+						sender.inventory.addItem(*Registry.Item.getAllItems())
+						return@executes 1
 					}
-				}
-				multiLiteralArgument(nodeName = "itemId", *Registry.Item.getItemIds()) {
-					integerArgument("variation", 0, 100, true) {
-						playerExecutor { player, args ->
-							val itemId = args.getUnchecked<String>("itemId")!!
-							val variationId = args.getOrDefaultUnchecked("variation", -1)!!
-							val item = Registry.Item.getItem(itemId, variationId)
-							if (item == null) {
-								val backup = Registry.Item.getItem(itemId, 0)!!
-								player.sendMessage(Component.text("The ")
-									.color(NamedTextColor.RED)
-									.append(backup.displayName().color(NamedTextColor.RED).hoverEvent(Registry.Item.getItem(itemId, 0)!!))
-									.append(Component.text(" does not have a variation id $variationId"))
-								)
-								return@playerExecutor
-							}
-							player.inventory.addItem(item)
-						}
-					}
-				}
-			}
-			literalArgument("changerarity") {
-				withRequirement { sender: CommandSender -> sender.isOp }
-				multiLiteralArgument(nodeName = "rarity", *Rarity.values().map { rarity -> rarity.name.lowercase() }.toTypedArray()) {
-					playerExecutor { player, args ->
-						val rarity = Rarity.valueOf((args["rarity"] as String).uppercase())
-						val heldItem = player.inventory.itemInMainHand
-						val selectedSlot = player.inventory.heldItemSlot
-						if (!isInfinityItem(heldItem)) {
-							player.sendMessage(Component.text("Cannot change rarity for item ")
-								.color(NamedTextColor.RED)
-								.append(Component.translatable(heldItem).color(NamedTextColor.RED).hoverEvent(heldItem))
-								.append(Component.text(" because it isn't an InfinityItem!").color(NamedTextColor.RED))
-							)
-							return@playerExecutor
-						}
-						val itemId = heldItem.itemMeta.persistentDataContainer[InfinityItem.ITEM_ID_KEY, PersistentDataType.STRING]!!
-						val variation = heldItem.itemMeta.persistentDataContainer[InfinityItem.VARIATION_ID, PersistentDataType.INTEGER]!!
-						val item = Registry.Item.getItem(itemId, variation)
-						player.inventory.setItem(selectedSlot, item!!.updateRarityTo(rarity))
-					}
-				}
-			}
-			literalArgument("upgrade") {
-				withRequirement { sender: CommandSender -> sender.isOp }
-				playerExecutor { player, _ ->
-					// Check if there's an InfinityItem in the main hand
+				)
+			)
+			.then(LiteralArgumentBuilder.literal<CommandSourceStack>("upgrade")
+				.requires(requirePlayerOperator)
+				.executes { ctx ->
+					val player = getSenderAsPlayer(ctx)
 					val heldItem = player.inventory.itemInMainHand
 					val selectedSlot = player.inventory.heldItemSlot
 					if (!isInfinityItem(heldItem)) {
@@ -106,7 +88,7 @@ object InfinityCommand {
 							.append(Component.translatable(heldItem).color(NamedTextColor.RED).hoverEvent(heldItem))
 							.append(Component.text(" because it isn't an InfinityItem!").color(NamedTextColor.RED))
 						)
-						return@playerExecutor
+						return@executes 1
 					}
 					// Upgrade the item
 					val itemId = heldItem.itemMeta.persistentDataContainer.get(InfinityItem.ITEM_ID_KEY, PersistentDataType.STRING)!!
@@ -127,11 +109,13 @@ object InfinityCommand {
 						)
 						.append(Component.text("!").color(NamedTextColor.GREEN))
 					)
+					return@executes 1
 				}
-			}
-			literalArgument("downgrade") {
-				withRequirement { sender: CommandSender -> sender.isOp }
-				playerExecutor { player, _ ->
+			)
+			.then(LiteralArgumentBuilder.literal<CommandSourceStack>("downgrade")
+				.requires(requirePlayerOperator)
+				.executes { ctx ->
+					val player = getSenderAsPlayer(ctx);
 					// Check if there's an InfinityItem in the main hand
 					val heldItem = player.inventory.itemInMainHand
 					val selectedSlot = player.inventory.heldItemSlot
@@ -141,7 +125,7 @@ object InfinityCommand {
 							.append(Component.translatable(heldItem).color(NamedTextColor.RED).hoverEvent(heldItem))
 							.append(Component.text(" because it isn't an InfinityItem!").color(NamedTextColor.RED))
 						)
-						return@playerExecutor
+						return@executes 1
 					}
 					// Downgrade the item
 					val itemId = heldItem.itemMeta.persistentDataContainer.get(InfinityItem.ITEM_ID_KEY, PersistentDataType.STRING)!!
@@ -162,25 +146,18 @@ object InfinityCommand {
 						)
 						.append(Component.text("!").color(NamedTextColor.RED))
 					)
+					return@executes 1
 				}
-			}
-			literalArgument("teleport") {
-				withRequirement { sender: CommandSender -> sender.isOp }
-				worldArgument("world") {
-					playerExecutor { player, args ->
-						val targetWorld = args["world"] as World
-						val currentGamemode = player.getGamemode()
-						val targetGamemode = Gamemode.getFromKey(targetWorld.key)
-						if (currentGamemode == targetGamemode) {
-							player.teleport(Location(targetWorld, player.location.x, player.location.y, player.location.z))
-						} else {
-							player.switchGamemode(PlayerTeleportEvent.TeleportCause.PLUGIN, targetWorld, Location(targetWorld, player.location.x, player.location.y, player.location.z))
-						}
-					}
-					locationArgument("location") {
-						playerExecutor { player, args ->
-							val targetWorld = args["world"] as World
-							val targetLocation = args["location"] as Location
+			)
+			.then(LiteralArgumentBuilder.literal<CommandSourceStack>("teleport")
+				.requires(requirePlayerOperator)
+				.then(RequiredArgumentBuilder.argument<CommandSourceStack, ResourceLocation>("world", DimensionArgument.dimension())
+					.then(RequiredArgumentBuilder.argument<CommandSourceStack, Coordinates>("location", Vec3Argument.vec3())
+						.executes { ctx ->
+							val player = getSenderAsPlayer(ctx)
+							val targetWorld = DimensionArgument.getDimension(ctx, "world").world
+							val vec3 = Vec3Argument.getVec3(ctx, "location")
+							val targetLocation = Location(null, vec3.x, vec3.y, vec3.z)
 							val currentGamemode = Gamemode.getFromKey(player.world.key)
 							val targetGamemode = Gamemode.getFromKey(targetWorld.key)
 							if (currentGamemode == targetGamemode) {
@@ -188,72 +165,29 @@ object InfinityCommand {
 							} else {
 								player.switchGamemode(PlayerTeleportEvent.TeleportCause.PLUGIN, targetWorld, targetLocation)
 							}
+							return@executes 1
 						}
-					}
-				}
-			}
-			literalArgument("gamemode") {
-				multiLiteralArgument(nodeName = "gamemode", "infinity", "minecraft") {
-					playerExecutor { player, args ->
-						when (args["gamemode"] as String) {
-							"infinity" -> {
-								if (player.getGamemode() != Gamemode.INFINITY) {
-									player.switchGamemode(PlayerTeleportEvent.TeleportCause.PLUGIN)
-								} else {
-									player.sendMessage(Component.text("You cannot execute this command right now as you are already playing ")
-										.color(NamedTextColor.RED)
-										.append(MiniMessage.miniMessage().deserialize("<gradient:#18e1f0:#de18f0>Minecraft Infinity</gradient>"))
-									)
-								}
-							}
-							"minecraft" -> {
-								if (player.getGamemode() != Gamemode.MINECRAFT) {
-									player.switchGamemode(PlayerTeleportEvent.TeleportCause.PLUGIN)
-								} else {
-									player.sendMessage(Component.text("You cannot execute this command right now as you are already playing ")
-										.color(NamedTextColor.RED)
-										.append(Component.text("Minecraft").color(NamedTextColor.GREEN))
-									)
-								}
-							}
+					)
+					.executes { ctx ->
+						val player = getSenderAsPlayer(ctx)
+						val targetWorld = DimensionArgument.getDimension(ctx, "world").world
+						val currentGamemode = player.getGamemode()
+						val targetGamemode = Gamemode.getFromKey(targetWorld.key)
+						if (currentGamemode == targetGamemode) {
+							player.teleport(Location(targetWorld, player.location.x, player.location.y, player.location.z))
+						} else {
+							player.switchGamemode(PlayerTeleportEvent.TeleportCause.PLUGIN, targetWorld, Location(targetWorld, player.location.x, player.location.y, player.location.z))
 						}
+						return@executes 1
 					}
-				}
-			}
-			literalArgument("defaultgamemode") {
-				multiLiteralArgument(nodeName = "gamemode", "infinity", "minecraft") {
-					playerExecutor { player, args ->
-						val chosenGamemode = args["gamemode"] as String
-						when (chosenGamemode) {
-							"infinity" -> player.persistentDataContainer.set(Keys.DEFAULT_GAMEMODE.get(), PersistentDataType.STRING, chosenGamemode)
-							"minecraft" -> player.persistentDataContainer.set(Keys.DEFAULT_GAMEMODE.get(), PersistentDataType.STRING, chosenGamemode)
-						}
-						player.sendMessage(Component.text("Set default gamemode to ")
-							.color(NamedTextColor.GRAY)
-							.append(Component.text(chosenGamemode.capitalize())
-								.color(when (chosenGamemode) {
-									"infinity" -> NamedTextColor.LIGHT_PURPLE
-									"minecraft" -> NamedTextColor.GREEN
-									else -> NamedTextColor.GRAY
-								}
-								)
-							)
-							.append(Component.text("!").color(NamedTextColor.GRAY))
-						)
-					}
-				}
-				literalArgument("reset") {
-					playerExecutor { player, _ ->
-						player.persistentDataContainer.remove(Keys.DEFAULT_GAMEMODE.get())
-						player.sendMessage(Component.text("Reset default gamemode!").color(NamedTextColor.RED))
-					}
-				}
-			}
-			literalArgument("startstory") {
-				withPermission("infinity.startstory")
-				playerExecutor { player, _ ->
+				)
+			)
+			.then(LiteralArgumentBuilder.literal<CommandSourceStack>("startstory")
+				.requires(requirePlayer.and { stack -> stack.source.getBukkitSender(stack).hasPermission("infinity.startstory") })
+				.executes { ctx ->
+					val player = getSenderAsPlayer(ctx)
 					Infinity.INSTANCE.playerPermissions.getOrDefault(player.uniqueId, player.addAttachment(Infinity.INSTANCE)).setPermission("infinity.startstory", false)
-					CommandAPI.updateRequirements(player)
+					player.updateCommands()
 					player.terminateStoryTitleTask()
 					player.persistentDataContainer.set(Keys.STORY_STARTED.get(), PersistentDataType.BOOLEAN, true)
 					player.sendMessage(Component.text().content("Story started! Have fun!").color(NamedTextColor.LIGHT_PURPLE).build())
@@ -264,13 +198,156 @@ object InfinityCommand {
 
 					// Initiate story start sequence
 					StoryHandler.startIntroduction(player)
+					return@executes 1
 				}
-			}
+			)
+		)
+
+		for (itemId in itemIds) {
+			Reflection.getDedicatedServer().commands.dispatcher.register(LiteralArgumentBuilder.literal<CommandSourceStack>("infinity")
+				.then(LiteralArgumentBuilder.literal<CommandSourceStack>("give")
+					.requires(requirePlayerOperator)
+					.then(LiteralArgumentBuilder.literal<CommandSourceStack>(itemId)
+						.then(RequiredArgumentBuilder.argument<CommandSourceStack, Int>("variation", IntegerArgumentType.integer(0, 100))
+							.executes { ctx ->
+								val player = getSenderAsPlayer(ctx)
+								val variationId = ctx.getArgument("variation", Int::class.java)
+								val item = Registry.Item.getItem(itemId, variationId)
+								if (item == null) {
+									val backup = Registry.Item.getItem(itemId, 0)!!
+									player.sendMessage(Component.text("The ")
+										.color(NamedTextColor.RED)
+										.append(backup.displayName().color(NamedTextColor.RED).hoverEvent(Registry.Item.getItem(itemId, 0)!!))
+										.append(Component.text(" does not have a variation id $variationId"))
+									)
+									return@executes 1
+								}
+								player.inventory.addItem(item)
+								return@executes 1
+							}
+						)
+						.executes { ctx ->
+							val player = getSenderAsPlayer(ctx)
+							val item = Registry.Item.getItem(itemId, -1)!!
+							player.inventory.addItem(item)
+							return@executes 1
+						}
+					)
+				)
+			)
 		}
+
+		for (rarity in Rarity.entries.map { rarity -> rarity.name.lowercase() }.toTypedArray()) {
+			Reflection.getDedicatedServer().commands.dispatcher.register(LiteralArgumentBuilder.literal<CommandSourceStack>("infinity")
+				.then(LiteralArgumentBuilder.literal<CommandSourceStack>("changerarity")
+					.requires(requirePlayerOperator)
+					.executes { ctx ->
+						val player = getSenderAsPlayer(ctx)
+						val rarity = Rarity.valueOf(rarity.uppercase())
+						val heldItem = player.inventory.itemInMainHand
+						val selectedSlot = player.inventory.heldItemSlot
+						if (!isInfinityItem(heldItem)) {
+							player.sendMessage(Component.text("Cannot change rarity for item ")
+								.color(NamedTextColor.RED)
+								.append(Component.translatable(heldItem).color(NamedTextColor.RED).hoverEvent(heldItem))
+								.append(Component.text(" because it isn't an InfinityItem!").color(NamedTextColor.RED))
+							)
+							return@executes 1
+						}
+						val itemId = heldItem.itemMeta.persistentDataContainer[InfinityItem.ITEM_ID_KEY, PersistentDataType.STRING]!!
+						val variation = heldItem.itemMeta.persistentDataContainer[InfinityItem.VARIATION_ID, PersistentDataType.INTEGER]!!
+						val item = Registry.Item.getItem(itemId, variation)
+						player.inventory.setItem(selectedSlot, item!!.updateRarityTo(rarity))
+						return@executes 1
+					}
+				)
+			)
+		}
+
+		for (gamemode in arrayOf("infinity", "minecraft")) {
+			Reflection.getDedicatedServer().commands.dispatcher.register(LiteralArgumentBuilder.literal<CommandSourceStack>("infinity")
+				.then(LiteralArgumentBuilder.literal<CommandSourceStack>("gamemode")
+					.then(LiteralArgumentBuilder.literal<CommandSourceStack>(gamemode)
+						.requires(requirePlayer)
+						.executes { ctx ->
+							val player = getSenderAsPlayer(ctx)
+							when (gamemode) {
+								"infinity" -> {
+									if (player.getGamemode() != Gamemode.INFINITY) {
+										player.switchGamemode(PlayerTeleportEvent.TeleportCause.PLUGIN)
+									} else {
+										player.sendMessage(Component.text("You cannot execute this command right now as you are already playing ")
+											.color(NamedTextColor.RED)
+											.append(MiniMessage.miniMessage().deserialize("<gradient:#18e1f0:#de18f0>Minecraft Infinity</gradient>"))
+										)
+									}
+								}
+
+								"minecraft" -> {
+									if (player.getGamemode() != Gamemode.MINECRAFT) {
+										player.switchGamemode(PlayerTeleportEvent.TeleportCause.PLUGIN)
+									} else {
+										player.sendMessage(Component.text("You cannot execute this command right now as you are already playing ")
+											.color(NamedTextColor.RED)
+											.append(Component.text("Minecraft").color(NamedTextColor.GREEN))
+										)
+									}
+								}
+							}
+							return@executes 1
+						}
+					)
+				)
+			)
+		}
+
+		for (gamemodeOption in arrayOf("infinity", "minecraft", "reset")) {
+			Reflection.getDedicatedServer().commands.dispatcher.register(LiteralArgumentBuilder.literal<CommandSourceStack>("infinity")
+				.then(LiteralArgumentBuilder.literal<CommandSourceStack?>("defaultgamemode")
+					.requires(requirePlayer)
+					.then(LiteralArgumentBuilder.literal<CommandSourceStack?>(gamemodeOption)
+						.executes { ctx ->
+							val player = getSenderAsPlayer(ctx)
+							if (gamemodeOption == "infinity" || gamemodeOption == "minecraft") {
+								when (gamemodeOption) {
+									"infinity" -> player.persistentDataContainer.set(Keys.DEFAULT_GAMEMODE.get(), PersistentDataType.STRING, gamemodeOption)
+									"minecraft" -> player.persistentDataContainer.set(Keys.DEFAULT_GAMEMODE.get(), PersistentDataType.STRING, gamemodeOption)
+								}
+								player.sendMessage(Component.text("Set default gamemode to ")
+									.color(NamedTextColor.GRAY)
+									.append(Component.text(gamemodeOption.capitalize())
+										.color(when (gamemodeOption) {
+											"infinity" -> NamedTextColor.LIGHT_PURPLE
+											"minecraft" -> NamedTextColor.GREEN
+											else -> NamedTextColor.GRAY
+										}
+										)
+									)
+									.append(Component.text("!").color(NamedTextColor.GRAY))
+								)
+							} else {
+								player.persistentDataContainer.remove(Keys.DEFAULT_GAMEMODE.get())
+								player.sendMessage(Component.text("Reset default gamemode!").color(NamedTextColor.RED))
+							}
+							return@executes 1
+						}
+					)
+				)
+			)
+		}
+
 	}
 
 	private fun isInfinityItem(item: ItemStack): Boolean {
 		return item.hasItemMeta() && item.itemMeta.persistentDataContainer.has(InfinityItem.ITEM_ID_KEY, PersistentDataType.STRING)
+	}
+
+	private fun getWrongExecutorException(): CommandSyntaxException {
+		return CommandSyntaxException(SimpleCommandExceptionType(LiteralMessage("You cannot execute this command!")), LiteralMessage("You cannot execute this command!"))
+	}
+
+	private fun getSenderAsPlayer(ctx: CommandContext<CommandSourceStack>): Player {
+		return ctx.source.source.getBukkitSender(ctx.source) as Player
 	}
 
 }
