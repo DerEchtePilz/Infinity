44455440e4156447d2acfec5fd40864ebb399b94
DerEchtePilz <tlfm90@gmail.com>
2023-11-23 01:40:13 +0100

    Clean-up build.gradle.kts (last commit with test tasks: 98416e02)

--- infinity-server/build.gradle.kts
+++ infinity-server/build.gradle.kts
@@ -1,13 +1,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-import org.gradle.api.tasks.testing.logging.TestLogEvent
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
-	kotlin("jvm") version "1.9.0"
 	id("com.github.johnrengelman.shadow") version "8.1.1"
 	id("xyz.jpenilla.run-paper") version "2.1.0"
 	id("io.papermc.paperweight.userdev") version "1.5.5"
@@ -45,14 +39,7 @@ val paperVersion: String by project
 
 dependencies {
 	implementation(project(":infinity-api"))
-	implementation("dev.jorel:commandapi-bukkit-shade:$commandAPIVersion")
-	compileOnly("dev.jorel:commandapi-bukkit-kotlin:$commandAPIVersion")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
 	compileOnly("com.google.code.gson:gson:2.10.1")
-	testImplementation("org.mockito:mockito-core:4.6.1")
-	testImplementation("com.github.seeseemelk:MockBukkit-v1.20:3.19.2")
-	testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	paperweight.paperDevBundle(paperVersion)
 
 	configurations.testRuntimeClasspath {
@@ -66,34 +53,6 @@ tasks.withType<JavaCompile> {
 
 tasks.withType<Javadoc> {
 	options.encoding = "UTF-8"
-}
-
-testing {
-	suites {
-		val test by getting(JvmTestSuite::class) {
-			useJUnitJupiter()
-		}
-		register<JvmTestSuite>("integrationTest") {
-			dependencies {
-				implementation(project())
-			}
-			targets {
-				all {
-					testTask.configure {
-						shouldRunAfter(test)
-					}
-				}
-			}
-		}
-	}
-}
-
-tasks.withType<Test> {
-	testLogging {
-		exceptionFormat = TestExceptionFormat.FULL
-		events = setOf(TestLogEvent.PASSED, TestLogEvent.FAILED, TestLogEvent.STANDARD_ERROR, TestLogEvent.STANDARD_OUT, TestLogEvent.SKIPPED)
-		showStandardStreams = true
-	}
 }
 
 tasks.withType<ProcessResources> {
@@ -110,17 +69,9 @@ tasks.withType<ProcessResources> {
 	}
 }
 
-tasks.withType<KotlinCompile> {
-	compilerOptions.languageVersion.set(KotlinVersion.KOTLIN_1_9)
-	compilerOptions.jvmTarget.set(JvmTarget.JVM_17)
-}
-
 tasks.withType<ShadowJar> {
 	dependencies {
-		include(dependency("dev.jorel:commandapi-bukkit-shade:$commandAPIVersion"))
-		include(dependency("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion"))
 		include(dependency("io.github.derechtepilz:infinity-api:${project.version}"))
 	}
-	relocate("dev.jorel.commandapi", "io.github.derechtepilz.commandapi")
 	minimize()
 }
